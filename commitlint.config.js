b5bcbd144509fc7d889eeacf433a9d8c3f1f595c
Quang Hiep <quanghiep03198@gmail.com>
2024-02-14 16:19:25 +0700

    chore: pre-commit linted stage config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,23 +1,23 @@
-import { RuleConfigCondition, RuleConfigSeverity, TargetCaseType } from '@commitlint/types'
+const { RuleConfigSeverity } = require('@commitlint/types')
 
-export const config = {
+module.exports = {
 	parserPreset: 'conventional-changelog-conventionalcommits',
 	rules: {
-		'body-leading-blank': [RuleConfigSeverity.Warning, 'always'] as const,
-		'body-max-line-length': [RuleConfigSeverity.Error, 'always', 100] as const,
-		'footer-leading-blank': [RuleConfigSeverity.Warning, 'always'] as const,
-		'footer-max-line-length': [RuleConfigSeverity.Error, 'always', 100] as const,
-		'header-max-length': [RuleConfigSeverity.Error, 'always', 100] as const,
-		'header-trim': [RuleConfigSeverity.Error, 'always'] as const,
+		'body-leading-blank': [RuleConfigSeverity.Warning, 'always'],
+		'body-max-line-length': [RuleConfigSeverity.Error, 'always', 100],
+		'footer-leading-blank': [RuleConfigSeverity.Warning, 'always'],
+		'footer-max-line-length': [RuleConfigSeverity.Error, 'always', 100],
+		'header-max-length': [RuleConfigSeverity.Error, 'always', 100],
+		'header-trim': [RuleConfigSeverity.Error, 'always'],
 		'subject-case': [
 			RuleConfigSeverity.Error,
 			'never',
 			['sentence-case', 'start-case', 'pascal-case', 'upper-case']
-		] as [RuleConfigSeverity, RuleConfigCondition, TargetCaseType[]],
-		'subject-empty': [RuleConfigSeverity.Error, 'never'] as const,
-		'subject-full-stop': [RuleConfigSeverity.Error, 'never', '.'] as const,
-		'type-case': [RuleConfigSeverity.Error, 'always', 'lower-case'] as const,
-		'type-empty': [RuleConfigSeverity.Error, 'never'] as const,
+		],
+		'subject-empty': [RuleConfigSeverity.Error, 'never'],
+		'subject-full-stop': [RuleConfigSeverity.Error, 'never', '.'],
+		'type-case': [RuleConfigSeverity.Error, 'always', 'lower-case'],
+		'type-empty': [RuleConfigSeverity.Error, 'never'],
 		'type-enum': [
 			RuleConfigSeverity.Error,
 			'always',
@@ -34,7 +34,7 @@ export const config = {
 				'style',
 				'test'
 			]
-		] as [RuleConfigSeverity, RuleConfigCondition, string[]]
+		]
 	},
 	prompt: {
 		questions: {
