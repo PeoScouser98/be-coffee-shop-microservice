7e294036b6ad19c922ad983f454a6ba52775692b
Quang Hiep <quanghiep03198@gmail.com>
2024-02-17 00:13:22 +0700

    chore: database module config

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,20 +1,17 @@
 import { Module } from '@nestjs/common'
-import { MongooseModule } from '@nestjs/mongoose'
 import { ConfigModule, ConfigService } from '@nestjs/config'
-import configuration from '../configs'
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
 	providers: [ConfigService],
 	imports: [
-		ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
 		MongooseModule.forRootAsync({
 			imports: [ConfigModule],
 			useFactory: (configService: ConfigService) => {
 				return {
-					uri: configService.get<string>('database.uri'),
-					dbName: configService.get<string>('database.dbName'),
-					maxPoolSize: configService.get<number>('database.maxPoolSize'),
-					connectTimeoutMS: configService.get<number>('database.connectTimeoutMS')
+					uri: configService.get<string>('MONGO_URI'),
+					maxPoolSize: 100,
+					connectTimeoutMS: 5000
 				}
 			},
 
